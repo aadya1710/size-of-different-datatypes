f410ddbb39fb2661f127121ce5a9558351d9c966
aadya1710 <77448543+aadya1710@users.noreply.github.com>
2021-08-11 14:47:06 +0530

    Create sizeofdatatypes.cpp
    this will tell the sixe of different datatypes

---
+++ sizeofdatatypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Size of int is : "<<sizeof(a)<<"bytes"<<endl;
+    float b;
+    cout<<"Size of float is : "<<sizeof(b)<<"bytes"<<endl;
+    char c;
+    cout<<"Size of char is : "<<sizeof(c)<<"bytes"<<endl;
+    double d;
+    cout<<"Size of double is : "<<sizeof(d)<<"bytes"<<endl;
+
+    return 0;
+}
